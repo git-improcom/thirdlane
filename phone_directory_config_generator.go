e5f49781733d89a34b4decf56acbe629b115232f
Alex Romanchuk <alexr@improcom.com>
2019-01-22 11:50:15 -0500

    Initial commit

---
+++ phone_directory_config_generator.go
@@ -0,0 +1,26 @@
+
+/*
+ * Copyright (c) 2019.
+ * The information in this file and any accompanying documentation is the
+ * property of IMPROCOM, Inc and is confidential and/or privileged. This
+ * file and any accompanying documentation is intended to be reviewed only
+ * by the individual or organization who has written permission from
+ * IMPROCOM, Inc. If you are not the intended recipient or an authorized
+ * representative of the intended recipient, you are hereby notified that
+ * any review, dissemination or copying of this file, if any, or the
+ * information contained herein is prohibited. If you have received this
+ * in error, please immediately notify:
+ * IMPROCOM, Inc
+ * 1803 Gravesend Neck road
+ * Brooklyn, NY, 11229 USA
+ * Phone: +1-718-769-3400
+ * @author Alex Romanchuk - alexr@improcom.com - Improcom INC
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello\n")
+}
